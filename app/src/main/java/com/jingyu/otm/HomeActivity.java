e39124b1d1f43aaef0438bc0d2a9e01ef4860130
eigen <jingyu.pang@tufts.edu>
2021-12-09 17:52:45 -0500

    added navigation and fragments

--- app/src/main/java/com/jingyu/otm/HomeActivity.java
+++ app/src/main/java/com/jingyu/otm/HomeActivity.java
@@ -1,14 +1,37 @@
 package com.jingyu.otm;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
+import com.jingyu.otm.databinding.ActivityHomeBinding;
 
 public class HomeActivity extends AppCompatActivity {
+//    private ActivityHomeBinding binding;
+    private NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        navView.setItemIconTintList(null);
+
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        navController = navHostFragment.getNavController();
+        NavigationUI.setupWithNavController(navView, navController);
+        NavigationUI.setupActionBarWithNavController(this, navController);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        return super.onSupportNavigateUp();
     }
 }
